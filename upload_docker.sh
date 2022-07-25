b0cb9fe4a855bac3e3fe68332ddf0de97e17755d
khairahscorner <airahyusuff@gmail.com>
2022-07-25 03:35:11 +0100

    feat: task#4 - upload image to dockerhub

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=khairahscorner/project4
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker image tag project4/ml_api $dockerpath:ml_api
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath:ml_api
