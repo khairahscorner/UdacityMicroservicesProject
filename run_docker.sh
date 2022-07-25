d92db263d6b9a8980ed89d89db3b280087974221
khairahscorner <airahyusuff@gmail.com>
2022-07-25 02:16:18 +0100

    feat: add docker script + fix to test on cloud9

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build -t project4/ml_api .
 
 # Step 2: 
 # List docker images
+docker images
 
 # Step 3: 
 # Run flask app
+docker run -p 8000:80 project4/ml_api
