9d2e94dece43136486f612d34b72d887e5262c11
khairahscorner <airahyusuff@gmail.com>
2022-07-25 18:02:42 +0100

    feat: task#5 attempt

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,20 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=khairahscorner/project4
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl get nodes
+kubectl create deployment project4 --image=$dockerpath:ml_api
+kubectl expose deployment web --type=NodePort --port=8000
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
+export POD_NAME=$(kubectl get pods -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
+echo Name of the Pod: $POD_NAME
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward pod/$POD_NAME --address 0.0.0.0 8000:80
